d6beb773709936aec8a23b9981d5304ac120817a
Susana Poveda <susanapoveda@iMac-de-Susana-2.local>
2022-08-03 17:26:07 -0500

    Collection sections mobile set up

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,14 +1,19 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import './Collection.css';
 import CollectionHeader, {CollectionView} from "../components/CollectionHeader";
 import CollectionPlants from "../components/CollectionPlants";
 import Badges from "../components/Badges";
 import {AppContext, AppValidActions} from "../context";
+import {DeviceTypes} from "../hooks/useWindowSize";
 
 export interface ICollectionProps {}
 
 const Collection: React.FunctionComponent<ICollectionProps> = (props) => {
   const {state, dispatch} = useContext(AppContext);
+  // Manages the sections to expand on mobile devices.
+  const [showBadges, setShowBadges] = useState(true);
+  const [showCollection, setShowCollection] = useState(false);
+  const [showInteractions, setShowInteractions] = useState(false);
 
   /** Gets and saves the user data only if it is not already saved. */
   // TODO: Fetch correct user data.
@@ -32,15 +37,60 @@ const Collection: React.FunctionComponent<ICollectionProps> = (props) => {
     dispatch({type: AppValidActions.GET_USER_DATA, payload: {userData: data}});
   };
 
+  /** Manages the badges section if the user expands it on mobile devices. */
+  const expandBadges = () => {
+    setShowBadges(true);
+    setShowCollection(false);
+    setShowInteractions(false);
+  };
+
+  /** Manages the Collection if the user expands it on mobile devices. */
+  const expandCollection = () => {
+    setShowBadges(false);
+    setShowCollection(true);
+    setShowInteractions(false);
+  };
+
+  /** Manages the Interactions section if the user expands it on mobile devices. */
+  const expandInteractions = () => {
+    setShowBadges(false);
+    setShowCollection(false);
+    setShowInteractions(true);
+  };
+
+  /** Renders the section selected on mobile devices. */
+  const getExpandedSection = () => {
+    if(showBadges) {
+      return <div className='mobile-section-container'><Badges/></div>;
+    } else if(showCollection) {
+      return <CollectionPlants/>;
+    } else if(showInteractions) {
+      return <div className='collection-interactions'> Interactions </div>;
+    }
+  };
+
   return (
     <main className="collection-page">
       <CollectionHeader view={CollectionView.OTHERS} />
-      <Badges />
+      {
+        state.deviceType === DeviceTypes.MOBILE?
+          <>
+            <div className='collection-option-container'>
+              <button className='button-open-section collection-option' onClick={expandBadges}> Badges </button>
+              <button className='button-open-section collection-option' onClick={expandCollection}> Collection </button>
+              <button className='button-open-section collection-option' onClick={expandInteractions}> Interactions </button>
+            </div>
 
-      <CollectionPlants />
-      <div className='collection-interactions'>
-        Interactions
-      </div>
+            { getExpandedSection() }
+          </>
+          :
+          <>
+            <Badges />
+            <CollectionPlants />
+            <div className='collection-interactions'> Interactions </div>
+          </>
+      }
+
     </main>
   );
 }
