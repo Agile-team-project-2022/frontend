e733d9f3fdb252b155e5f07861a7a94781c0f1df
Susana Poveda <susanapoveda@iMac-de-Susana-2.local>
2022-08-10 14:18:27 -0500

    Comments style adjust.

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import './Comments.css';
 import {CommentData} from "../context";
+import {parseDate} from "../helpers";
 
 export interface ICommentsProps {
   inputComments: CommentData[],
@@ -15,13 +16,26 @@ const Comments: React.FunctionComponent<ICommentsProps> = ({inputComments, onUpd
     setComments(inputComments);
   }, [inputComments]);
 
+  /** Prepares the formatting of the content as an array before rendering. */
+  const formatComment = (text: string) => {
+    return text.replace('\n\n', '\n');
+  };
+
   return (
     <div className="comments-container">
       {
         comments.map((item, index) => {
           return (
             <div className='comment-item' key={`comment-item-${item.id}`}>
-              Comment
+              <span className='metadata'>{parseDate(item.createdAt)} by {item.authorId} --- TODO: replace with name</span>
+              <p>
+                {formatComment(item.content).split('\n').map((itemParagraph, indexParagraph) => {
+                  return itemParagraph !== ''?
+                    <Fragment key={`paragraph-item-comment-${item.id}-${indexParagraph}`}>{itemParagraph}<br/><br/></Fragment>
+                    :
+                    '';
+                })}
+              </p>
             </div>
           );
         })
