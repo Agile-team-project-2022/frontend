7fe29f791af0084a72b5f1b57e79af85ae3cd866
Susana Poveda <susanapoveda@iMac-de-Susana-2.local>
2022-08-14 15:04:02 -0500

    Profile plant set mobile.

--- src/pages/PlantProfile.tsx
+++ src/pages/PlantProfile.tsx
@@ -10,14 +10,22 @@ import PublishedPost from "../components/PublishedPost";
 import NewPost from "../components/NewPost";
 import GalleryPreview from "../components/GalleryPreview";
 import WaterSchedule from "../components/WaterSchedule";
+import Badges from "../components/Badges";
+import CollectionPlants from "../components/CollectionPlants";
+import CollectionInteractions from "../components/CollectionInteractions";
+import {DeviceTypes} from "../hooks/useWindowSize";
 
 export interface IPlantProfileProps {}
 
 const PlantProfile: React.FunctionComponent<IPlantProfileProps> = () => {
   const {plantId, ownerId} = useParams();
-  const {state: {userData: {plants}, BASE_URL}} = useContext(AppContext);
+  const {state: {userData: {plants}, deviceType, BASE_URL}} = useContext(AppContext);
   const [plantData, setPlantData] = useState<PlantData>();
   const [plantHeaderData, setPlantHeaderData] = useState<PlantHeaderData>({id: -1, name: '', imageFile: '', species: '', followers: 0});
+  // Manages the sections to expand on mobile devices.
+  const [showGallery, setShowGallery] = useState(true);
+  const [showData, setShowData] = useState(false);
+  const [showPublications, setShowPublications] = useState(false);
 
   /** Gets the full plant data. */
   useEffect(() => {
@@ -41,41 +49,105 @@ const PlantProfile: React.FunctionComponent<IPlantProfileProps> = () => {
     // eslint-disable-next-line
   }, [plants]);
 
+  /** Manages the badges section if the user expands it on mobile devices. */
+  const expandGallery = () => {
+    setShowGallery(true);
+    setShowData(false);
+    setShowPublications(false);
+  };
+
+  /** Manages the Collection if the user expands it on mobile devices. */
+  const expandData = () => {
+    setShowGallery(false);
+    setShowData(true);
+    setShowPublications(false);
+  };
+
+  /** Manages the Interactions section if the user expands it on mobile devices. */
+  const expandPublications = () => {
+    setShowGallery(false);
+    setShowData(false);
+    setShowPublications(true);
+  };
+
+  /** Renders the section selected on mobile devices. */
+  const getExpandedSection = () => {
+    if(showGallery) return <div className='mobile-section-container'><GalleryPreview imageFiles={['', '', '', '', '']} /> {/* TODO: Pass the correct array of plant images. */}</div>;
+    else if(showData) return <div className='mobile-section-container'><WaterSchedule /></div>;
+    else if(showPublications) return <div className='mobile-section-container'>{ getPublications() }</div>;
+  };
+
+  /** Renders the publications and posts. */
+  const getPublications = () => {
+    return (
+      <>
+        <Suspense>
+          {
+            plantData !== undefined && plantData.posts.length > 0?
+              plantData.posts.map((item, index) => {
+                return <PublishedPost post={item} key={`published-post-item-${item.id}`} />;
+              })
+              :
+              <p> No Posts to show yet for owner Id: {ownerId} </p>
+          }
+        </Suspense>
+        <h2 className='section-title publications-section-title'>Publications</h2>
+      </>
+    );
+  };
+
   return (
     <main className="plant-profile-page">
       <ProfileHeader plantData={plantHeaderData} view={CollectionView.OWNER} />
 
-      <div className='plant-profile-data-container'>
-        <WaterSchedule />
-      </div>
+      {
+        deviceType === DeviceTypes.MOBILE?
+          <>
+            <div className='collection-option-container'>
+              <button className={`button-open-section collection-option ${showGallery? 'selected-collection-option' : ''}`}
+                      onClick={expandGallery}
+              >
+                Gallery
+              </button>
+              <button className={`button-open-section collection-option ${showData? 'selected-collection-option' : ''}`}
+                      onClick={expandData}
+              >
+                Data
+              </button>
+              <button className={`button-open-section collection-option ${showPublications? 'selected-collection-option' : ''}`}
+                      onClick={expandPublications}
+              >
+                Publications
+              </button>
+            </div>
 
-      <div className='page-content-container'>
-        <section className='publications-container'>
-          <GalleryPreview imageFiles={['', '', '', '', '']} /> {/* TODO: Pass the correct array of plant images. */}
-        </section>
+            { getExpandedSection() }
+          </>
+          :
+          <>
+            <div className='plant-profile-data-container'>
+              <WaterSchedule />
+            </div>
 
-        <section className='publications-container'>
-          <Suspense> <NewPost />  </Suspense>
-        </section>
+            <div className='page-content-container'>
+              <section className='publications-container'>
+                <GalleryPreview imageFiles={['', '', '', '', '']} /> {/* TODO: Pass the correct array of plant images. */}
+              </section>
 
-        <div className='section-divisor'> </div>
+              <section className='publications-container'>
+                <Suspense> <NewPost />  </Suspense>
+              </section>
 
-        <section className='publications-container'>
-          <Suspense>
-            {
-              plantData !== undefined && plantData.posts.length > 0?
-                plantData.posts.map((item, index) => {
-                  return <PublishedPost post={item} key={`published-post-item-${item.id}`} />;
-                })
-                :
-                <p> No Posts to show yet for owner Id: {ownerId} </p>
-            }
-          </Suspense>
-          <h2 className='section-title publications-section-title'>Publications</h2>
-        </section>
-      </div>
+              <div className='section-divisor'> </div>
 
-      <div> </div>
+              <section className='publications-container'>
+                { getPublications() }
+              </section>
+            </div>
+
+            <div> More data </div>
+          </>
+      }
     </main>
   );
 }
