c46e47331b3f5e7b4cb42cc9f39b5acc69f124f7
Susana Poveda <susanapoveda@iMac-de-Susana-2.local>
2022-08-13 21:20:20 -0500

    Expand images from gallery preview enabled.

--- src/components/GalleryPreview.tsx
+++ src/components/GalleryPreview.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './GalleryPreview.css';
 import {CheckEncodedImage} from '../helpers';
 import {LazyLoadImage} from "react-lazy-load-image-component";
-import defaultPlantImg from "../assets/example-plant-2.jpeg"; // TODO: Select correct default image.
+import defaultPlantImg from "../assets/example-plant-2.jpeg";
+import GalleryExpanded from "./GalleryExpanded";
 
 export interface IGalleryPreviewProps {
   imageFiles: string[]
 }
 
 const GalleryPreview: React.FunctionComponent<IGalleryPreviewProps> = ({imageFiles}) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const selectImage = (imageFile: string) => {
+    setSelectedImage(imageFile);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div className='gallery-preview-container'>
       <h2 className='section-title'>Photo gallery <span>({imageFiles.length})</span></h2>
@@ -16,7 +29,10 @@ const GalleryPreview: React.FunctionComponent<IGalleryPreviewProps> = ({imageFil
         {
           imageFiles.map((item, index) => {
             return (
-              <div className='gallery-img-container' key={`gallery-preview-item-${index}`}>
+              <div className='gallery-img-container'
+                   key={`gallery-preview-item-${index}`}
+                   onClick={() => selectImage(item)}
+              >
                 {/* TODO: change index by an unique image ID. */}
                 <LazyLoadImage src={CheckEncodedImage(item)? item : defaultPlantImg} alt='Plant gallery item' />
               </div>
@@ -29,6 +45,8 @@ const GalleryPreview: React.FunctionComponent<IGalleryPreviewProps> = ({imageFil
           <div> </div>
         </button>
       </div>
+
+      {modalIsOpen? <GalleryExpanded imageFile={selectedImage} onClose={closeModal} /> : ''}
     </div>
   );
 }
