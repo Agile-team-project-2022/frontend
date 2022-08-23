393b5f2208ad9d7a233b7fcda208f084495f940f
Susana Poveda <susanapoveda@iMac-de-Susana-2.local>
2022-08-23 12:07:25 -0500

    Complete settings feature.

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,32 @@
 import React, {ChangeEvent, useContext} from 'react';
 import './Settings.css';
 import {AppContext, AppValidActions} from "../context";
+import img1 from '../assets/login-img-1.jpg';
+import img2 from '../assets/login-img-2.jpg';
+import img3 from '../assets/login-img-3.jpg';
+import img4 from '../assets/flower.jpeg';
+import resetImg from '../assets/reset.png';
 
 export interface ISettingsProps {}
 
 const Settings: React.FunctionComponent<ISettingsProps> = () => {
   const {state: {settings: {fontSize, grayscale, contrast, brightness}}, dispatch} = useContext(AppContext);
+
+  /** Revert all the changes done. */
+  const handleReset = () => {
+    const bodyCollection = document.getElementsByTagName('html');
+    const body = bodyCollection[0];
+    body.style.fontSize = '1rem';
+    body.style.filter = '';
+    dispatch({type: AppValidActions.CHANGE_SETTINGS, payload: {
+        newSettings: {
+          fontSize: 1,
+          contrast: 100,
+          brightness: 100,
+          grayscale: 0
+        }
+      }});
+  };
 
   /** Updates the value of font size base on the slider. */
   const handleOnChangeFontSize = (e: ChangeEvent<HTMLInputElement>) => {
@@ -82,53 +103,63 @@ const Settings: React.FunctionComponent<ISettingsProps> = () => {
       <h2 className='section-title-modal'>Settings</h2>
 
       <div className='settings-content'>
-        <div className='settings-option-item font-size-option'>
+        <div className='settings-option-item'>
+          <div className='settings-option-img'><img src={img1} alt='Plant' /></div>
           <span className='slider-label'>Font size</span>
           <input type='range'
                  min={0.8} max={1.2} step={0.01}
                  value={fontSize}
                  onChange={(e) => handleOnChangeFontSize(e)}
-                 className='font-size-slider'
+                 className='settings-slider'
           />
           <span className='min-slider-label'>Smaller</span>
           <span className='max-slider-label'>Larger</span>
         </div>
 
-        <div className='settings-option-item font-size-option'>
+        <div className='settings-option-item'>
+          <div className='settings-option-img'><img src={img2} alt='Plant' /></div>
           <span className='slider-label'>Grayscale</span>
           <input type='range'
                  min={0} max={100} step={0.1}
                  value={grayscale}
                  onChange={(e) => handleOnChangeGrayscale(e)}
-                 className='font-size-slider'
+                 className='settings-slider'
           />
           <span className='min-slider-label'>Colorful</span>
           <span className='max-slider-label'>Gray</span>
         </div>
 
-        <div className='settings-option-item font-size-option'>
+        <div className='settings-option-item'>
+          <div className='settings-option-img'><img src={img3} alt='Plant' /></div>
           <span className='slider-label'>Contrast</span>
           <input type='range'
-                 min={95} max={110} step={0.1}
+                 min={80} max={120} step={0.1}
                  value={contrast}
                  onChange={(e) => handleOnChangeContrast(e)}
-                 className='font-size-slider'
+                 className='settings-slider'
           />
           <span className='min-slider-label'>Low</span>
           <span className='max-slider-label'>High</span>
         </div>
       </div>
 
-      <div className='settings-option-item font-size-option'>
+      <div className='settings-option-item'>
+        <div className='settings-option-img'><img src={img4} alt='Plant' /></div>
         <span className='slider-label'>Brightness</span>
         <input type='range'
                min={70} max={120} step={0.1}
                value={brightness}
                onChange={(e) => handleOnChangeBrightness(e)}
-               className='font-size-slider'
+               className='settings-slider'
         />
         <span className='min-slider-label'>Dark</span>
         <span className='max-slider-label'>Bright</span>
+      </div>
+
+      <div className='reset-button'>
+        <div> </div>
+        <span>Reset to default</span>
+        <img src={resetImg} alt='Reset' onClick={handleReset} />
       </div>
 
     </div>
